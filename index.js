d5764b3521a762b254a1e15915f31317f11e71c1
fontilan <68748054+fontilan@users.noreply.github.com>
2022-09-27 15:05:38 +0200

    style: single quotes

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-const inputEl = document.getElementById("number-input");
-const resultEl = document.getElementById("result-msg");
-const buttonEl = document.getElementById("check-btn");
+const inputEl = document.getElementById('number-input');
+const resultEl = document.getElementById('result-msg');
+const buttonEl = document.getElementById('check-btn');
 
-buttonEl.addEventListener("click", function () {
+buttonEl.addEventListener('click', function () {
   convert(inputEl.value);
 });
 
-// let number;
-
 let arabic = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
 let roman = [
-  "M",
-  "CM",
-  "D",
-  "CD",
-  "C",
-  "XC",
-  "L",
-  "XL",
-  "X",
-  "IX",
-  "V",
-  "IV",
-  "I",
+  'M',
+  'CM',
+  'D',
+  'CD',
+  'C',
+  'XC',
+  'L',
+  'XL',
+  'X',
+  'IX',
+  'V',
+  'IV',
+  'I',
 ];
 
 function convert(number) {
-  let result = "";
+  let result = '';
   for (let i = 0; i < roman.length; i++) {
     while (number >= arabic[i]) {
       result += roman[i];
